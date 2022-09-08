22405473496d83c3b67b6c911cb36e5d37879f74
sushan <86766049+sushanshakya77@users.noreply.github.com>
2022-09-08 09:06:22 +0545

    Delete backend directory

--- backend/@types/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace Express {
-  export interface Request {
-    refreshToken: string;
-    accessToken: string;
-    user: any;
-  }
-}
--- backend/app/config/database.ts
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect("mongodb://localhost:27017/travelog", () => {
-  console.log("connected to db");
-});
